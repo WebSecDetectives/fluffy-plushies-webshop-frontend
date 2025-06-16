71ec9f3ecd741905dd75276e099ecdc17d225dbe
MuhammadAliKhan <ali88311@hotmail.com>
2025-06-16 23:56:32 +0200

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'fluffy_plushies_web_app';
+}
