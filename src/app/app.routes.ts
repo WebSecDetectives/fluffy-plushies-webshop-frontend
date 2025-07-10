7b0173414aa45679b92708a6bb466935b7baf12e
MuhammadAliKhan <ali88311@hotmail.com>
2025-07-10 13:11:18 +0200

    building home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,20 @@
 import { Routes } from '@angular/router';
 import { CreateAccount } from './create-account/create-account';
 import { UpdateAccount } from './update-account/update-account';
+import { Home } from './home/home';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/create',
-    pathMatch: 'full'
+    component: Home
   },
   {
     path: 'create',
     component: CreateAccount
+  },
+  {
+    path: 'update',
+    component: UpdateAccount
   },
   {
     path: 'update',
