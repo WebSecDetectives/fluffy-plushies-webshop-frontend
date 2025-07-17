3e9d843ed0e28d0361e9c964c51efb69422a2244
MuhammadAliKhan <ali88311@hotmail.com>
2025-07-18 01:38:33 +0200

    home page built

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,10 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
