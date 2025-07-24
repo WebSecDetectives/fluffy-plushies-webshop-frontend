b27b27e2d800fc0784358069761b1e691012f42f
MuhammadAliKhan <ali88311@hotmail.com>
2025-07-24 15:27:35 +0200

    fixed a lot of minor things

--- src/enviroments/firebase.config.ts
+++ src/enviroments/firebase.config.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAxOtOdHiI5RHyP7UYAToaX3Ry0kjiBSQc",
   authDomain: "fluffy-plushies-webshop.firebaseapp.com",
   projectId: "fluffy-plushies-webshop",
-  storageBucket: "fluffy-plushies-webshop.firebasestorage.app",
+  storageBucket: "fluffy-plushies-webshop.appspot.com",
   messagingSenderId: "827529865010",
   appId: "1:827529865010:web:711fe366ec98902203992b",
   measurementId: "G-YEXEJZ9G6M"
